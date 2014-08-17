786c73edd88b7ac80473fa60c80d868250827305
Ralph Krimmel <mail@postefix.de>
2014-08-17 18:20:55 +0200

    structure of approach

--- work.sh
+++ work.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-vim -p document.tex chapters/*/*.tex
+
+vim -p \
+	chapters/approach/approach.tex \
+	chapters/casestudy/casestudy.tex \
+	chapters/conclusion/conclusion/conclusion.tex \
+	chapters/discussion/discussion.tex \
+	chapters/implementation/implementation.tex \
+	chapters/introduction/introduction.tex
+
+
+
