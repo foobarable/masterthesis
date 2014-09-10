f1b42e08a8803b565271e8ef768f430fadabb316
Ralph Krimmel <mail@postefix.de>
2014-09-10 18:28:33 +0200

    restructuring, task distance substitution matrix

--- work.sh
+++ work.sh
@@ -2,12 +2,12 @@
 
 
 vim -p \
+	chapters/introduction/introduction.tex \
+	chapters/foundations/foundations.tex \
 	chapters/approach/approach.tex \
+	chapters/implementation/implementation.tex \
 	chapters/casestudy/casestudy.tex \
 	chapters/conclusion/conclusion/conclusion.tex \
-	chapters/discussion/discussion.tex \
-	chapters/implementation/implementation.tex \
-	chapters/introduction/introduction.tex
 
 
 
