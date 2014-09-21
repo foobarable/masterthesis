87d821f34fb8423ec080cd4176f669455cacb508
sonnengeschein <hanna.holderied@gmail.com>
2014-09-21 11:01:19 +0200

    merged

--- work.sh
+++ work.sh
@@ -2,12 +2,13 @@
 
 
 vim -p \
+	document.tex \
+	chapters/introduction/introduction.tex \
+	chapters/foundations/foundations.tex \
 	chapters/approach/approach.tex \
+	chapters/implementation/implementation.tex \
 	chapters/casestudy/casestudy.tex \
 	chapters/conclusion/conclusion/conclusion.tex \
-	chapters/discussion/discussion.tex \
-	chapters/implementation/implementation.tex \
-	chapters/introduction/introduction.tex
 
 
 
